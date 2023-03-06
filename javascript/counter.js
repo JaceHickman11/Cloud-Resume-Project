7695dc58caeba21e3359d4f01e207e7d0a4a51f1
Jace Hickman <120608247+JaceHickman11@users.noreply.github.com>
2023-03-06 08:11:57 -0500

    Update counter.js to interact with DynamoDB

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,11 +1,53 @@
-let count = localStorage.getItem('pageCount');
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
 
-if (!count) {
-  count = 0;
-}
+// Set the region
+AWS.config.update({region: 'us-east-1'});
 
-count++;
-localStorage.setItem('pageCount', count);
+// Create the DynamoDB service object
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-let counterElement = document.getElementById('counter');
-counterElement.innerText = 'Page views: ' + count;
+// Get the current count from DynamoDB
+dynamodb.getItem({
+  TableName: 'PageViews',
+  Key: {
+    'counterName': {S: 'index.html'}
+  }
+}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    let count = data.Item.Count.N;
+
+    // If the count is not set, initialize it to 0
+    if (!count) {
+      count = 0;
+    }
+
+    // Increment the count and save it back to DynamoDB
+    count++;
+    dynamodb.updateItem({
+      TableName: 'PageViews',
+      Key: {
+        'counterName': {S: 'index.html'}
+      },
+      UpdateExpression: 'SET #count = :count',
+      ExpressionAttributeNames: {
+        '#count': 'Count'
+      },
+      ExpressionAttributeValues: {
+        ':count': {N: count.toString()}
+      }
+    }, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        console.log(data);
+      }
+    });
+
+    // Display the count on the webpage
+    let counterElement = document.getElementById('counter');
+    counterElement.innerText = 'Page views: ' + count;
+  }
+});
