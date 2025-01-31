00b6fd689e5ed1bd5bfc6afabda62b553a5e52da
Jace Hickman <hickmanj@localhost.localdomain>
2025-01-31 18:13:18 -0500

    Initial resource definitions, need to defined attributes for each

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@
         # Query resources and return values back
         # terraform output - query outputs.tf
 
-
 # 
 terraform {
   required_providers {
@@ -27,7 +26,60 @@ provider "aws" {
     region = "us-east-2"
 }
 
+resource "aws_s3_bucket" "primary_bucket" {
+    bucket = "jacehickman.com"
+}
+
+resource "aws_s3_bucket" "www_bucket" {
+    bucket = "www.jacehickman.com"
+}
+
+# Needs way more stuff
+resource "aws_cloudfront_distribution" "s3_distribution" {
+    provider = aws
+    default_cache_behavior {
+      
+    }
+    enabled = true
+    origin {
+      
+    }
+    restrictions {
+      
+    }
+    viewer_certificate {
+    
+    }
+
+}
+
+resource "aws_route53_zone" "my_dns" {
+  name = "jacehickman.com"
+}
+
+resource "aws_dynamodb_table" "visitor_table" {
+  name = "VisitorTable"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "CloudResumeAPI"
+}
+
+resource "aws_lambda_function" "updateItem_py" {
+  function_name = "updateItem"
+}
+
+# Commands to import my resources
+# terraform import aws_s3_bucket.primary_bucket jacehickman.com
+# terraform import aws_s3_bucket.www_bucket www.jacehickman.com
+# terraform import aws_cloudfront_distribution.s3_distribution E3EERE5S6HGZEH
+# terraform import aws_route53_zone.my_dns Z07797793UO05BKPV64D2
+# terraform import aws_dynamodb_table.visitor_table VisitorTable
+# terraform import aws_api_gateway_rest_api.api z3v2iubne2
+# terraform import aws_lambda_function.updateItem_py updateItem
+
 # data blocks only access the resource
+# data "aws_resource type" "resource_reference"
 data "aws_s3_bucket" "my_bucket" {
     bucket = "jacehickman.com"
 }
@@ -48,6 +100,6 @@ data "aws_api_gateway_rest_api" "api" {
     name = "CloudResumeAPI"
 }
 
-data "aws_lambda_function" "lambda_python" {
+data "aws_lambda_function" "updateItem_py" {
     function_name = "updateItem"
 }
